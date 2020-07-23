619ae9c1fe93d5aa95fb5252c1eaa4d679c0e995
mona <monaxwang@gmail.com>
2020-07-23 00:06:01 -0700

    More of Founder's requests have been completed.

--- js/h0tbot.js
+++ js/h0tbot.js
@@ -1,15 +1,27 @@
 let questions = [
   {
-    q: "Who is mother?",
-    a: "We owe much to mother. We were founded right here in San Francisco, all thanks to mother. She is Good."
+    q: "What is Corporate Fantasy Adventures, LLC?",
+    a: "Great question! We are leaders, changemakers, and role models. We fight for those at the margins. We fight for profit. And we fight for profit margins."
   },
   {
-    q:"I would like to give money to Corporate Fantasy Adventures, LLC",
-    a:"Corporate Fantasy Adventures, LLC appreciates your offer"
+    q:"I’m organizing a conference. How can I get a speaker from Corporate Fantasy Adventures, LLC?",
+    a:"Great question! We have many fantastic speakers available to you. Please get in touch via our email form at the bottom of the site."
   },
-    {
-    q:"What is Corporate Fantasy Adventures, LLC's preferred mode of transportation?",
-    a:"That would be the Segway personal scooter. <a href='https://store.segway.com/segway-ninebot-s-plus-miniplus-self-balancing'>Available for $849.99</a>"
+  {
+    q:"I’m a reporter and I’m wondering how I can get in touch with your press team at Corporate Fantasy Adventures, LLC?",
+    a:"Great question! Please reach us at pr@corporatefantasyad.ventures. Please note that response times are subject to change."
+  },
+  {
+    q:"I’m a hard-hitting investigative journalist and I suspect something underhanded is happening at your company. Why won’t you respond to my emails?",
+    a:"Great question!"
+  },
+  {
+    q:"How can I invest in your cryptocurrency, CorpCoin?",
+    a:"Great question! You can join us on our annual yacht retreat. We meet in the redwoods, then walk to a secret beach, where we are joined by fellow politicians and media figures. You will then be asked to pitch to our team of esteemed technologists, who will make projections."
+  },
+  {
+    q:"I am a Corporate Fantasy Adventures, LLC employee and I am having trouble finding information about your retirement plans.",
+    a:"Great question! We support our employees retiring. You can find more information at the bottom of your bimonthly pay stub."
   },
 ]
 let question;
